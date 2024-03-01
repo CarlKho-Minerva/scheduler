b099fe74728676ccd438b41192489f48acc85cd0
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-03-01 23:40:55 +0000

    anim alt fix 3.1

--- dataviz.py
+++ dataviz.py
@@ -46,11 +46,14 @@ date = st.selectbox('Which date would you like to see?', date_options, 100)
 country_options = covid['Country'].unique().tolist()
 country = st.multiselect('Which country would you like to see?', country_options, ['Brazil'])
 
+# Convert 'Date' column to datetime
+covid['Date'] = pd.to_datetime(covid['Date'])
+
 # Filter the COVID-19 dataset based on the selected countries
 covid = covid[covid['Country'].isin(country)]
 
 # Create an animated bar chart
-fig2 = px.bar(covid, x="Country", y="Confirmed", color="Country", range_y=[0,35000], animation_frame="Date", animation_group="Country")
+fig2 = px.bar(covid, x="Country", y="Confirmed", color="Country", range_y=[0,35000], animation_frame=covid["Date"].dt.strftime('%Y-%m-%d'), animation_group="Country")
 
 # Update the layout of the plot to include a 'Play' button
 fig2.update_layout(
