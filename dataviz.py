8635463d1c824e3fca9238258cfab0368df9554e
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-03-01 23:33:52 +0000

    anim alt fix

--- dataviz.py
+++ dataviz.py
@@ -3,21 +3,31 @@ import numpy as np
 import streamlit as st
 import time
 
+# Your actual data
+data = {
+    'Facebook': [2.2e9, 2.3e9, 2.4e9, 2.5e9, 2.6e9],
+    'YouTube': [1.6e9, 1.7e9, 1.8e9, 1.9e9, 2.0e9],
+    'WhatsApp': [1.5e9, 1.55e9, 1.6e9, 1.65e9, 1.7e9],
+    # Add other platforms...
+}
+
 fig, ax = plt.subplots()
 
-max_x = 5
-max_rand = 10
+max_x = len(data['Facebook'])  # Number of data points
+max_y = max(max(data.values()))  # Maximum y-value in your data
 
 x = np.arange(0, max_x)
-ax.set_ylim(0, max_rand)
-line, = ax.plot(x, np.random.randint(0, max_rand, max_x))
+ax.set_ylim(0, max_y)
+line, = ax.plot(x, data['Facebook'])  # Start with Facebook data
 the_plot = st.pyplot(plt)
 
 def init():  # give a clean slate to start
     line.set_ydata([np.nan] * len(x))
 
 def animate(i):  # update the y values (every 1000ms)
-    line.set_ydata(np.random.randint(0, max_rand, max_x))
+    platform = list(data.keys())[i % len(data)]  # Cycle through platforms
+    line.set_ydata(data[platform])  # Update y-values with platform data
+    ax.set_title(platform)  # Update title with current platform
     the_plot.pyplot(plt)
 
 init()
